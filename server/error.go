0a4dbf52d78678eeb97ae06c048fc95cf505d581
kotoyuuko <kokoro@core.moe>
2019-04-02 16:21:14 +0800

    new error message

--- server/error.go
+++ server/error.go
@@ -8,7 +8,7 @@ func ErrorResponse(code int, msg string) *Response {
 		Code:    code,
 		Charset: "utf-8",
 		MIME:    "text/html",
-		Content: "<h1>HTTP " + strconv.Itoa(code) + "</h1><p>" + msg + "</p>",
+		Content: "<h1>Bronya Boom!</h1><h4>Code " + strconv.Itoa(code) + "</h4><p>" + msg + "</p>",
 	}
 	return response
 }
